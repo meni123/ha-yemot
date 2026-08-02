06c6505a7a87e5408859469a57c65935629df393
meni123 <49362774+meni123@users.noreply.github.com>
2026-08-02 19:05:46 +0300

    Update config_flow.py

--- yemot_api/config_flow.py
+++ yemot_api/config_flow.py
@@ -2,7 +2,7 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 
-DOMAIN = "yemot_api"
+DOMAIN = "ha-yemot"
 
 class YemotConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
