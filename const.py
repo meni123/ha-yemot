5be2be9ff4143296cb49def77ebe60e44ce18e3f
meni123 <49362774+meni123@users.noreply.github.com>
2026-08-02 22:00:23 +0300

    Create const.py

---
+++ const.py
@@ -0,0 +1,10 @@
+"""Constants for the Yemot API integration."""
+
+DOMAIN = "yemot_api"
+
+# Yemot HaMashiach API URLs
+YEMOT_API_BASE_URL = "https://www.call2all.co.il/ym/api"
+UPDATE_EXTENSION_URL = f"{YEMOT_API_BASE_URL}/UpdateExtension"
+UPLOAD_TEXT_FILE_URL = f"{YEMOT_API_BASE_URL}/UploadTextFile"
+RUN_TZINTUK_URL = f"{YEMOT_API_BASE_URL}/RunTzintuk"
+SEND_TTS_URL = f"{YEMOT_API_BASE_URL}/SendTTS"
