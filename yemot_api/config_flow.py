33ac5e0b9da86797b6d308a1a455aaf7110fb8d6
meni123 <49362774+meni123@users.noreply.github.com>
2026-08-02 18:59:17 +0300

    Create config_flow.py

---
+++ yemot_api/config_flow.py
@@ -0,0 +1,25 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+
+DOMAIN = "yemot_api"
+
+class YemotConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            return self.async_create_entry(title="ימות המשיח", data=user_input)
+
+        data_schema = vol.Schema({
+            vol.Required("external_url", default="https://YOUR_HA_DOMAIN"): str,
+            vol.Required("api_token", default="123456"): str,
+            vol.Required("yemot_manager_token", default="0771234567:123456"): str,
+            vol.Optional("allowed_ips", default=""): str
+        })
+
+        return self.async_show_form(
+            step_id="user", data_schema=data_schema, errors=errors
+        )
